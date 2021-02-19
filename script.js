e02694986be0e53d1bd2ddb9f13d8c0cc52c5f9b
Jose William Ingram Ramirez <jwramz@hotmail.com>
2021-02-18 20:28:01 -0600

    Modificaciones a la página
    Resultado final del trabajo solicitado

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+ 	r = document.getElementById("rate").value;
+	y = document.getElementById("years").value;
+ 
+    i = p*y*(r/100);
+	anio = new Date().getFullYear() + parseInt(y);
+	 
+	if (p < 1) {
+		 window.alert("Enter a positive number");
+		 document.getElementById("principal").focus();
+	 }
+	 else{
+		result.innerHTML = "If you deposit " + "<mark>" + p + "</mark>" + "," + "</br>" + "at an interest rate of " + "<mark>" + r + "%.</mark>" +  "</br>" + " You will recive an amount of " + "<mark>" +i.toFixed(2) + "</mark>" +  "," + "</br>" + " in the year " + "<mark>" + anio + "</mark>" ;
+	 }
+}
+
+function muestraPorcentaje(porcentaje)       
+{
+	var valor = document.getElementById("porcentaje");
+	valor.innerHTML = porcentaje + "%";
 }
         
